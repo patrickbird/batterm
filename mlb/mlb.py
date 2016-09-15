472001179368f85c947349d1ab78d90582452851
Patrick Bird <patbird@gmail.com>
2016-09-14 21:49:03 -0700

    Refactor into Scoreboard class.

--- mlb/mlb.py
+++ mlb/mlb.py
@@ -7,117 +7,125 @@ import shutil
 import sys
 from multiprocessing.dummy import Pool as ThreadPool
 
-def create_empty_linescore():
-    return {                
-            'r': {
-                'home': '',
-                'away': ''
-            },
-            'h': {
-                'home': '',
-                'away': ''
-            },
-            'e': {
-                'home': '',
-                'away': ''
-            }
-    }
-
-class ScorecardManager:
+class ScoreboardManager:
     _scoreboards = {}
 
-    def _get_scoreboard_url(date):
-        "Get the master scorecard URL of a given day."
-        return date,'http://m.mlb.com/gdcross/components/game/mlb/year_{0}/month_{1}/day_{2}/master_scoreboard.json'.format(date.year, str(date.month).zfill(2), str(date.day).zfill(2))
+    def get_scoreboard(date):
+        dates=[]
+        for new_date in ScoreboardManager._get_date_range(date):
+            if new_date not in ScoreboardManager._scoreboards:
+                dates.append(new_date)
 
-    def _get_scoreboard_url_chunk(date):
-        return [ScorecardManager._get_scoreboard_url(date + datetime.timedelta(days=1) - datetime.timedelta(days=x)) for x in range(3)]
+        if len(dates) > 0 and date not in ScoreboardManager._scoreboards:
+            ScoreboardManager._scoreboards.update([(s.date, s) for s in ScoreboardManager._get_scoreboards(dates)])
 
-    def _get_scoreboard_dictionary(url):
-        scoreboard = json.loads(requests.get(url).text)['data']['games']
-        return datetime.date(int(scoreboard['year']), int(scoreboard['month']), int(scoreboard['day'])), scoreboard
+        return ScoreboardManager._scoreboards[date]
 
-    def _get_scoreboard_dictionaries(urls):
-        pool = ThreadPool(len(urls))
+    def _get_date_range(date):
+        return [date + datetime.timedelta(days=1) - datetime.timedelta(days=x) for x in range(3)]
 
-        results = pool.map(ScorecardManager._get_scoreboard_dictionary, urls)
+    def _get_scoreboards(dates):
+        pool = ThreadPool(len(dates))
+
+        results = pool.map(Scoreboard, dates)
 
         pool.close()
         pool.join()
 
         return results
 
-    def get_scoreboard(date):
-        urls=[]
-        for scoreboard_date, scoreboard_url in ScorecardManager._get_scoreboard_url_chunk(date):
-            if scoreboard_date not in ScorecardManager._scoreboards:
-                urls.append(scoreboard_url)
+class Scoreboard:
+    def __init__(self, date):
+        self.date = date
+        self.url = self._get_scoreboard_url()
+        self.scoreboard = self._get_scoreboard_dictionary()
 
-        if len(urls) > 0 and date not in ScorecardManager._scoreboards:
-            ScorecardManager._scoreboards.update(ScorecardManager._get_scoreboard_dictionaries(urls))
+    def _create_empty_linescore():
+        return {                
+                'r': {
+                    'home': '',
+                    'away': ''
+                },
+                'h': {
+                    'home': '',
+                    'away': ''
+                },
+                'e': {
+                    'home': '',
+                    'away': ''
+        }
+    }
+        
+    def _get_scoreboard_url(self):
+        "Get the master scorecard URL of a given day."
+        return 'http://m.mlb.com/gdcross/components/game/mlb/year_{0}/month_{1}/day_{2}/master_scoreboard.json'.format(self.date.year, str(self.date.month).zfill(2), str(self.date.day).zfill(2))
 
-        return ScorecardManager._scoreboards[date]
+    def _get_scoreboard_dictionary(self):
+        return json.loads(requests.get(self.url).text)['data']['games']
 
-def get_deciding_pitcher_line(pitcher):
-    return '{} ({}, {}-{})'.format(pitcher['name_display_roster'], pitcher['era'], pitcher['wins'], pitcher['losses'])
+    def get_deciding_pitcher_line(self, pitcher):
+        return '{} ({}, {}-{})'.format(pitcher['name_display_roster'], pitcher['era'], pitcher['wins'], pitcher['losses'])
 
-def print_boxscore(game):
-    boxscore = []
-    score = game['linescore'] if 'linescore' in game else create_empty_linescore()
-    status = game['status']['status']
+    def get_boxscores(self):
+        return [self.get_boxscore(x) for x in self.scoreboard['game']]
 
-    if status != 'In Progress':
-        boxscore.append(status)
-    else:
-        boxscore.append(game['status']['inning_state'][:3] + ' ' + game['status']['inning'])
-        boxscore[-1] += '  {}-{}, {} {}'.format(game['status']['b'], game['status']['s'], game['status']['o'], 'Outs' if int(game['status']['o']) != 1 else 'Out')
+    def get_boxscore(self, game):
+        boxscore = []
+        score = game['linescore'] if 'linescore' in game else self._create_empty_linescore()
+        status = game['status']['status']
 
-    boxscore.append('----------------------')
-    boxscore.append('| {:3} | {:>2} | {:>2} | {:>2} |'.format(game['away_name_abbrev'], score['r']['away'], score['h']['away'], score['e']['away']))
-    boxscore.append('| {:3} | {:>2} | {:>2} | {:>2} |'.format(game['home_name_abbrev'], score['r']['home'], score['h']['home'], score['e']['home']))
-    boxscore.append('----------------------')
+        if status != 'In Progress':
+            boxscore.append(status)
+        else:
+            boxscore.append(game['status']['inning_state'][:3] + ' ' + game['status']['inning'])
+            boxscore[-1] += '  {}-{}, {} {}'.format(game['status']['b'], game['status']['s'], game['status']['o'], 'Outs' if int(game['status']['o']) != 1 else 'Out')
 
-    if game['status']['status'] == 'Final':
-        boxscore.append('W: {}'.format(get_deciding_pitcher_line(game['winning_pitcher']))) if 'winning_pitcher' in game else ''
-        boxscore.append('L: {}'.format(get_deciding_pitcher_line(game['losing_pitcher']))) if 'losing_pitcher' in game else ''
+        boxscore.append('----------------------')
+        boxscore.append('| {:3} | {:>2} | {:>2} | {:>2} |'.format(game['away_name_abbrev'], score['r']['away'], score['h']['away'], score['e']['away']))
+        boxscore.append('| {:3} | {:>2} | {:>2} | {:>2} |'.format(game['home_name_abbrev'], score['r']['home'], score['h']['home'], score['e']['home']))
+        boxscore.append('----------------------')
 
-        if 'save_pitcher' in game and game['save_pitcher']['name_display_roster'] != '':
-            boxscore.append('S: {} ({})'.format(game['save_pitcher']['name_display_roster'], game['save_pitcher']['saves']))
+        if game['status']['status'] == 'Final':
+            boxscore.append('W: {}'.format(self.get_deciding_pitcher_line(game['winning_pitcher']))) if 'winning_pitcher' in game else ''
+            boxscore.append('L: {}'.format(self.get_deciding_pitcher_line(game['losing_pitcher']))) if 'losing_pitcher' in game else ''
 
-    elif status == 'In Progress':
-        runners = game['runners_on_base']
-        boxscore.append('  {}    '.format("\u2b25" if 'runner_on_2b' in runners else "\u2b26"))
-        boxscore[-1] += 'P:  {}'.format(game['pitcher']['name_display_roster'])
+            if 'save_pitcher' in game and game['save_pitcher']['name_display_roster'] != '':
+                boxscore.append('S: {} ({})'.format(game['save_pitcher']['name_display_roster'], game['save_pitcher']['saves']))
 
-        boxscore.append('{}   '.format("\u2b25" if 'runner_on_3b' in runners else "\u2b26"))
-        boxscore[-1] += '{}  AB: {}'.format("\u2b25" if 'runner_on_1b' in runners else "\u2b26", game['batter']['name_display_roster'])
+        elif status == 'In Progress':
+            runners = game['runners_on_base']
+            boxscore.append('  {}    '.format("\u2b25" if 'runner_on_2b' in runners else "\u2b26"))
+            boxscore[-1] += 'P:  {}'.format(game['pitcher']['name_display_roster'])
 
-        #boxscore.append(' {}-{} {} Out(s)'.format(game['status']['b'], game['status']['s'], game['status']['o']))
-        boxscore.append('')
+            boxscore.append('{}   '.format("\u2b25" if 'runner_on_3b' in runners else "\u2b26"))
+            boxscore[-1] += '{}  AB: {}'.format("\u2b25" if 'runner_on_1b' in runners else "\u2b26", game['batter']['name_display_roster'])
 
-    return boxscore
+            #boxscore.append(' {}-{} {} Out(s)'.format(game['status']['b'], game['status']['s'], game['status']['o']))
+            boxscore.append('')
 
-def print_detailed_boxscore(game):
-    score = game['linescore']
-    inning_count = max(9, len(score['inning']))
-    
-    header = [str(x) for x in range(1, inning_count + 1)] + ['r', 'h', 'e']
-    away_runs = [x['away'] for x in score['inning']] + [score['r']['away'], score['h']['away'], score['e']['away']]
-    home_runs = [x.get('home', ' ') for x in score['inning']] + [score['r']['home'], score['h']['home'], score['e']['home']]
+        return boxscore
 
-    buf = []
-    buf.append(game['time_date'])
-    buf.append(game['location'] + ', ' + game['venue'])
-    buf.append('--------------' + ('-----' * len(header)))
-    buf.append(' {:11}'.format(game['status']['status']) + ' '.join(' {:>2} '.format(x) for x in header))
-    buf.append('--------------' + ('-----' * len(header)))
+    def print_detailed_boxscore(self, game):
+        score = game['linescore']
+        inning_count = max(9, len(score['inning']))
+        
+        header = [str(x) for x in range(1, inning_count + 1)] + ['r', 'h', 'e']
+        away_runs = [x['away'] for x in score['inning']] + [score['r']['away'], score['h']['away'], score['e']['away']]
+        home_runs = [x.get('home', ' ') for x in score['inning']] + [score['r']['home'], score['h']['home'], score['e']['home']]
 
-    buf.append(' {:11}'.format(game['away_team_name']) + ' '.join(' {:>2} '.format(x) for x in away_runs))
-    buf.append(' {:11}'.format(game['home_team_name']) + ' '.join(' {:>2} '.format(x) for x in home_runs))
-    buf.append('--------------' + ('-----' * len(header)))
-    buf.append('')
+        buf = []
+        buf.append(game['time_date'])
+        buf.append(game['location'] + ', ' + game['venue'])
+        buf.append('--------------' + ('-----' * len(header)))
+        buf.append(' {:11}'.format(game['status']['status']) + ' '.join(' {:>2} '.format(x) for x in header))
+        buf.append('--------------' + ('-----' * len(header)))
 
-    return buf
+        buf.append(' {:11}'.format(game['away_team_name']) + ' '.join(' {:>2} '.format(x) for x in away_runs))
+        buf.append(' {:11}'.format(game['home_team_name']) + ' '.join(' {:>2} '.format(x) for x in home_runs))
+        buf.append('--------------' + ('-----' * len(header)))
+        buf.append('')
+
+        return buf
 
 def get_team_boxscore(game, sel):
     players = game['liveData']['boxscore']['teams'][sel]['players']
@@ -218,17 +226,16 @@ class MlbShell(cmd.Cmd):
     date = datetime.date.today()
 
     def print_rhe():
-        scoreboard = ScorecardManager.get_scoreboard(MlbShell.date)
-        boxscores = [print_boxscore(x) for x in scoreboard['game']]
+        scoreboard = ScoreboardManager.get_scoreboard(MlbShell.date)
         
         print('')
-        print('{}-{}-{}'.format(scoreboard['year'], scoreboard['month'], scoreboard['day']))
+        print(scoreboard.date)
         print('')
 
-        print_boxscores(boxscores)
+        print_boxscores(scoreboard.get_boxscores())
 
     def do_box(self, arg):
-        scoreboard = ScorecardManager.get_scoreboard(MlbShell.date)
+        scoreboard = ScoreboardManager.get_scoreboard(MlbShell.date)
         scoreboard_game = scoreboard['game'][int(arg) - 1]
         game = json.loads(requests.get('http://statsapi.mlb.com/api/v1/game/' + scoreboard_game['game_pk']  + '/feed/live').text)
         boxscore = print_detailed_boxscore(scoreboard_game)
@@ -240,7 +247,7 @@ class MlbShell(cmd.Cmd):
         print(*team_boxscore, sep='\n')
 
     def do_plays(self, arg):
-        scoreboard = ScorecardManager.get_scoreboard(MlbShell.date)
+        scoreboard = ScoreboardManager.get_scoreboard(MlbShell.date)
         scoreboard_game = scoreboard['game'][int(arg) - 1]
         game = json.loads(requests.get('http://statsapi.mlb.com/api/v1/game/' + scoreboard_game['game_pk']  + '/feed/live').text)
 
