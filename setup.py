bd412cdfdcbd73287bf47e678cd3de87e1d07d9b
Patrick Bird <patbird@gmail.com>
2016-09-15 21:51:21 -0700

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='baspy',
+      version='1.0',
+      description='Get scoring information for MLB baseball games.',
+      author='Patrick Bird',
+      author_email='patbird@gmail.com',
+      url='https://github.com/patrickbird/baspy',
+      packages=['baspy'],
+)
+
